ea1ae00877fa0a49a889ffad45aaa717c9bc136b
Roman Meclazcke <137325701+romanmeclazcke@users.noreply.github.com>
2026-06-09 20:02:29 -0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+  groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string, string[]>();
+
+    for (const st of strs) {
+      const key = this.invertStrs(st);
+
+      if (!map.has(key)) {
+        map.set(key, []);
+      }
+
+      map.get(key)!.push(st);
+    }
+
+    return Array.from(map.values());
+  }
+
+  invertStrs(s: string): string {
+    return s
+      .split("")
+      .sort()
+      .join("");
+  }
+}
