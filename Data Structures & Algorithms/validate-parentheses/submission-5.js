68ebbc2535fb5d0bcafb15c403baa9e98e025e9d
Roman Meclazcke <137325701+romanmeclazcke@users.noreply.github.com>
2026-06-14 16:37:01 -0300

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+   isValid(s) {
+
+        let simb = {
+
+            "[": "]",
+
+            "{": "}",
+
+            "(": ")"
+
+        };
+
+        let stack = [];
+
+        for (let i = 0; i < s.length; i++) {
+
+            if (Object.keys(simb).includes(s[i])) {
+
+                stack.push(s[i]);
+
+                continue;
+
+            }
+
+            if (simb[stack.pop()] !== s[i]) {
+
+                return false;
+
+            }
+
+        }
+
+        return stack.length === 0;
+
+    }
+}
